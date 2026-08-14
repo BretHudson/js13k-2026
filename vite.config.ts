5dcacd4eb482f049ae464971980c08d7e4ea3bd9
Bret Hudson <bret@brethudson.com>
2026-08-13 22:30:12 -0700

    Actually set up Vite config to allow for shader HMR

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,38 @@ import glsl from 'vite-plugin-glsl';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-	plugins: [glsl({ minify: true }), viteSingleFile()],
+	plugins: [
+		{
+			name: 'configure response headers',
+			configureServer: (server) => {
+				server.middlewares.use((_req, res, next) => {
+					res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+					res.setHeader(
+						'Cross-Origin-Embedder-Policy',
+						'credentialless',
+					);
+					next();
+				});
+			},
+		},
+		{
+			name: 'shader-hmr',
+			handleHotUpdate: ({ file, server }) => {
+				if (file.endsWith('.wgsl')) {
+					server.ws.send({
+						type: 'custom',
+						event: 'shader-update',
+						data: { file },
+					});
+					return [];
+				}
+
+				return;
+			},
+		},
+		glsl({ minify: true }),
+		viteSingleFile(),
+	],
 	build: {
 		target: 'esnext',
 	},
